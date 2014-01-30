54344d1c4a52771b83295d2f0b9771ecbb5431d7
Brimborion <charles.guerin.rouard@gmail.com>
2014-01-30 12:25:54 +0100

    Lien revu entre creation_devoir.html et saisie_notes.php Début intégration devoir et notes dans saisie_notes.php

--- site/php/saisie_notes.php
+++ site/php/saisie_notes.php
@@ -1,5 +1,29 @@
 <?php
 	
+	//défini l'appelle de la page
+	if(isset($_SERVER["HTTP_REFERER"])) { 				//si vient d'une autre page 
+		$origin=explode('/',$_SERVER["HTTP_REFERER"]);	
+		switch ($origin[4]) {							//test par rapport au nom de la page d'origine
+			case 'creation_devoir.php':
+				//renvoyer les posts dans la BDD
+				$devoir['date']=$_POST['date'];
+				$devoir['matiere']=$_POST['matiere'];
+				$devoir['classe']=$_POST['classe'];
+				$devoir['devoir']=$_POST['devoir'];
+				break;
+			case 'saisie_notes.php':
+				//renvoyer les notes dans la BDD
+				//ouvre le menu
+				die ();
+				break;
+		}
+	}
+	
+	/*
+		Fonction qui prend un tableau en entrée et renvoie une ligne
+		de tableau html avec une cellule qui est l'élément du tableau
+		et deux cellules de saisie de texte.
+	*/
 	function tableauSaisie($tableauEleve){
 		foreach ($tableauEleve as $value){
 			echo "<tr>\n";
