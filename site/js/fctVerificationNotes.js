3bab24dd060df142cab6b8b34c8eb6183696960a
Brimborion <charles.guerin.rouard@gmail.com>
2014-02-05 11:31:50 +0100

    Commentaires fctVerificationNotes.js

--- site/js/fctVerificationNotes.js
+++ site/js/fctVerificationNotes.js
@@ -1,9 +1,14 @@
+/*
+	fonction qui ne prend pas d'arguments et qui permet de vérifier si les notes
+	(inputs avec name="note") sont valides.
+*/
+
 function verificationNotes () {
-					var notes=document.getElementsByClassName("note");
+					var notes=document.getElementsByClassName("note"); //renvoie toutes les notes dans un tableau
 					for(var i=0; i<notes.length; i++){
 						if ((notes[i].value!="abs" && isNaN(notes[i].value)) || (notes[i].value<0 || notes[i].value>20)){
-							notes[i].value="!corriger!";
-							var erreur=true;
+							notes[i].value="!corriger!"; //envoie un message dans la cellule fausse
+							var erreur=true; //vrai si une erreur de saisie est levée
 						}
 					}
 					if(erreur==true) {
