e0168079e385754dffe65c453917529c79951583
Brimborion <charles.guerin.rouard@gmail.com>
2014-02-09 08:42:18 +0100

    Fichier création des tables : OK maj diagramme de la BDD : utilisation de mysql workbench

--- site/config/bddENT.sql
+++ site/config/bddENT.sql
@@ -46,6 +46,15 @@ CREATE TABLE matiere (
 	PRIMARY KEY (idMatiere),
 	FOREIGN KEY (domaine_idDomaine) REFERENCES domaine (idDomaine)
 );
+
+/* Création de la table utilisateur_has_matiere */
+CREATE TABLE utilisateur_has_matiere (
+	utilisateur_idUtilisateur INTEGER UNSIGNED NOT NULL,
+	matiere_idMatiere INTEGER UNSIGNED NOT NULL,
+	PRIMARY KEY (utilisateur_idUtilisateur, matiere_idMatiere),
+	FOREIGN KEY (utilisateur_idUtilisateur) REFERENCES utilisateur (idUtilisateur),
+	FOREIGN KEY (matiere_idMatiere) REFERENCES matiere (idMatiere)
+);
 	
 /* Création de la table trimestre */
 CREATE TABLE trimestre (
@@ -84,3 +93,76 @@ CREATE TABLE utilisateur_has_statut (
 	FOREIGN KEY (statut_idStatut) REFERENCES statut (idStatut)
 );
 
+/* Création de la table typeVoie */
+CREATE TABLE typeVoie (
+	idTypeVoie INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+	typeVoie VARCHAR(20) NOT NULL,
+	PRIMARY KEY (idTypeVoie)
+);
+
+/* Création de la table codePostal */
+CREATE TABLE codePostal (
+	idCodePostal INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+	codePostal INTEGER NOT NULL,
+	PRIMARY KEY (idCodePostal)
+);
+
+/* Création de la table ville */
+CREATE TABLE ville (
+	idVille INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+	nomVille VARCHAR(45) NOT NULL,
+	codePostal_idCodePostal INTEGER NOT NULL,
+	PRIMARY KEY (idVille),
+	FOREIGN KEY (codePostal_idCodePostal) REFERENCES codePostal (idCodePostal)
+);
+
+/* Création de la table adresse */
+CREATE TABLE adresse (
+	idAdresse INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+	utilisateur_idUtilisateur INTEGER UNSIGNED NOT NULL,
+	typeVoie_idTypeVoie INTEGER UNSIGNED NOT NULL,
+	ville_idVille INTEGER UNSIGNED NOT NULL,
+	numero VARCHAR(20) NOT NULL,
+	voie VARCHAR(255) NOT NULL,
+	complement VARCHAR(255) NOT NULL,
+	PRIMARY KEY (idAdresse),
+	FOREIGN KEY (utilisateur_idUtilisateur) REFERENCES utilisateur (idUtilisateur),
+	FOREIGN KEY (typeVoie_idTypeVoie) REFERENCES typeVoie (idTypeVoie),
+	FOREIGN KEY (ville_idVille) REFERENCES ville (idVille)
+);
+
+/* Création de la table devoir */
+CREATE TABLE devoir (
+	idDevoir INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+	utilisateur_idUtilisateur INTEGER UNSIGNED NOT NULL,
+	classe_idClasse INTEGER UNSIGNED NOT NULL,
+	intitule VARCHAR(255) NOT NULL,
+	date DATE NOT NULL,
+	lienSujet VARCHAR(255),
+	lienCorige VARCHAR(255),
+	moyenne FLOAT NOT NULL,
+	PRIMARY KEY (idDevoir),
+	FOREIGN KEY (utilisateur_idUtilisateur) REFERENCES utilisateur (idUtilisateur),
+	FOREIGN KEY (classe_idClasse) REFERENCES classe (idClasse)
+);
+
+/* Création de la table devoir_has_matiere */
+CREATE TABLE devoir_has_matiere (
+	devoir_idDevoir INTEGER UNSIGNED NOT NULL,
+	matiere_idMatiere INTEGER UNSIGNED NOT NULL,
+	PRIMARY KEY (devoir_idDevoir, matiere_idMatiere),
+	FOREIGN KEY (devoir_idDevoir) REFERENCES devoir (idDevoir),
+	FOREIGN KEY (matiere_idMatiere) REFERENCES matiere (idMatiere)
+);
+
+/* Création de la table correctionDevoir */
+CREATE TABLE correctionDevoir (
+	utilisateur_idUtilisateur INTEGER UNSIGNED NOT NULL,
+	devoir_idDevoir INTEGER UNSIGNED NOT NULL,
+	note FLOAT NOT NULL,
+	commentaire TEXT,
+	PRIMARY KEY (utilisateur_idUtilisateur, devoir_idDevoir),
+	FOREIGN KEY (utilisateur_idUtilisateur) REFERENCES utilisateur (idUtilisateur),
+	FOREIGN KEY (devoir_idDevoir) REFERENCES devoir (idDevoir)
+);
+	
