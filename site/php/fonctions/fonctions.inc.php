cdd6ebefacdec05bd6c2e33acf3eaecd0f11d9c2
Brimborion <charles.guerin.rouard@gmail.com>
2014-02-11 10:32:26 +0100

    Création de fonctions.inc.php avec gestion de l'utf8 Intégration de la fonciton fctRenvoieUneListe.php dans fonctions.inc.php Modification css pout saisie_notes.html Ajout des icônes des menus

---
+++ site/php/fonctions/fonctions.inc.php
@@ -0,0 +1,37 @@
+<?php
+
+/* Indique au serveur php de renvoyer de l'utf8 */
+
+header( 'content-type: text/html; charset=utf-8' );
+
+
+
+/**
+	*Fonction renvoieUneListe prend pour argument un tableau généré par la base de donnée
+	*ainsi qu'une url avec query string de forme url/page?variable=
+	*		ex : renvoieUneListe($monTableau, "../php/page?classe=");
+	*@param array $tableauBDD Tableau contenant la liste des élèves d'une classe
+	*@param string $urlQuery Chaîne de caractères qui est une url avec le début
+	*						d'une query string
+	*@return string Liste en HTML de type ul avec des liens gros et centrés,
+	*				nécessite knacss
+*/
+
+function renvoieUneListe ($tableauBDD, $urlQuery) {
+		//Créé la liste
+		$result='<ul class = "unstyled ">'."\n";
+		//Boucle pour dérouler tous les éléments du tableau
+		/* $nbElements=count($tableauBDD);
+		for ($i=0; $i<$nbElements; $i++){ */
+		foreach ($tableauBDD as $value) {
+			//un élément de la liste
+			$result.="\t<li class = \"bigger mbm txtcenter\">\n";
+			//renvoie quelque chose comme : <a href="../php/page?classe=test"</a>
+			$result.="\t\t<a href=\"".$urlQuery.$value."\">".$value."</a>\n";
+			$result.="\t</li>\n";
+		
+		}
+		$result.='</ul>';
+		return $result;
+	}
+?>
