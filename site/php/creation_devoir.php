5636cf6efa5b12c2c69597e6d0a0d13fc09fafb8
Brimborion <charles.guerin.rouard@gmail.com>
2014-02-14 11:10:05 +0100

    Mise à jour schéma BDD

--- site/php/creation_devoir.php
+++ site/php/creation_devoir.php
@@ -3,6 +3,17 @@
 	autorisationPage('formateur');
 	
 	$classes=['ITStart','CDPN','DL'];
+	/* RECUPERATION DES CLASSES DE L'UTILISATEUR */
+	//Connexion à la BDD
+	$cnx = cnxBase ();
+	// S'il y a un problème de connexion on renvoie l'erreur
+	if (is_string($cnx)) {
+		erreur(ERREUR_CONNEXION_BDD);
+	}
+	
+	$sql = "SELECT * FROM utilisateur_has_matiere, utilisateur, matiere WHERE idUtilisateur = utilisateur_idUtilisateur AND matiere_idMatiere = idMatiere AND idUtilisateur ="$_SESSION[';
+	$result = $cnx->query($sql);
+	$tabRes = $result->fetchAll(PDO::FETCH_ASSOC);
 	$matieres=['JAVA','PHP','SQL'];
 	
 	$titrePage="Creation de Devoir";
