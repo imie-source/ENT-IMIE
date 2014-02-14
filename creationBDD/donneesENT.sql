4fee5972be482a7ae3bf01907df66d9a8ae2a6aa
Brimborion <charles.guerin.rouard@gmail.com>
2014-02-14 16:11:55 +0100

    ajout dans donneesENT.sql des differentes voies .

---
+++ creationBDD/donneesENT.sql
@@ -0,0 +1,13 @@
+/*Structure deuxieme partie de la table typeVoie*/
+
+
+INSERT INTO typeVoie (idTypeVoie, typeVoie) VALUES
+(1, 'Rue'),
+(2, 'Avenue'),
+(3, 'Boulevard'),
+(4, 'Bourg'),
+(5, 'Impasse'),
+(6, 'Chemin'),
+(7, 'Square'),
+(8, 'Route',),
+(9, 'Allée',);
