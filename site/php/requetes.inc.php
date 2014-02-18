6fbe3e721ff19cc2451a7cb232e96a0ffe0f0f2a
Brimborion <charles.guerin.rouard@gmail.com>
2014-02-18 11:05:30 +0100

    Mise à jour requetes.inc.php

--- site/php/requetes.inc.php
+++ site/php/requetes.inc.php
@@ -27,6 +27,79 @@ define('GET_UTILISATEUR', 'SELECT *
 							AND statut_idStatut = idStatut 
 							AND login =~');
 
+define('GET_MOYENNE_ELEVE','SELECT matiere.nomMatiere, ROUND(AVG(note),2) AS moyenne 
+								FROM classe, utilisateur_has_classe, utilisateur, correctionDevoir, utilisateur_has_matiere,  matiere  
+								WHERE classe.idClasse = utilisateur_has_classe.classe_idClasse 
+								AND utilisateur_has_classe.utilisateur_idUtilisateur = utilisateur.idUtilisateur 
+								AND utilisateur.idUtilisateur = correctionDevoir.utilisateur_idUtilisateur 
+								AND utilisateur.idUtilisateur = utilisateur_has_matiere.utilisateur_idUtilisateur 
+								AND utilisateur_has_matiere.matiere_idMatiere = matiere.idMatiere 
+								AND classe.idClasse = ~ GROUP BY matiere.idMatiere');
+
+define('GET_MOYENNE_CLASSE','SELECT appreciation 
+								FROM trimestre, appreciation, utilisateur_has_statut, utilisateur								
+								WHERE utilisateur.idUtilisateur =  ~
+								AND trimestre.idtrimestre = ~');
+								
+define('GET_APPRECIATION','SELECT appreciation 
+								FROM trimestre, appreciation, utilisateur_has_statut, utilisateur 
+								WHERE utilisateur.idUtilisateur =  ~ 
+								AND trimestre.idtrimestre = ~');
+
+define('GET_MOYENNE_CLASSE_DEVOIR','SELECT ROUND(AVG(note),2) AS moyenne 
+								FROM correctionDevoir, devoir, devoir_has_matiere, matiere 
+								WHERE correctionDevoir.devoir_idDevoir = devoir.idDevoir 
+								AND devoir.idDevoir = devoir_has_matiere.devoir_idDevoir 
+								AND devoir_has_matiere.matiere_idMatiere = matiere.idMatiere 
+								AND matiere.nomMatiere = ~
+								GROUP BY devoir.idDevoir');
+								
+define('GET_NOTE_UTILISATEUR','SELECT note 
+								FROM matiere, utilisateur_has_matiere, utilisateur, devoir, correctionDevoir 
+								WHERE matiere.idMatiere = utilisateur_has_matiere.matiere_idMatiere 
+								AND utilisateur_has_matiere.utilisateur_idUtilisateur = utilisateur.idUtilisateur 
+								AND utilisateur.idUtilisateur = devoir.utilisateur_idUtilisateur 
+								AND devoir.idDevoir = correctionDevoir.devoir_idDevoir 
+								AND matiere.nomMatiere = ~
+								AND utilisateur.idUtilisateur =  ~');
+								
+define('GET_INTITULE_DEVOIR','SELECT intitule 
+								FROM matiere, utilisateur_has_matiere, utilisateur, devoir, correctionDevoir 
+								WHERE matiere.idMatiere = utilisateur_has_matiere.matiere_idMatiere 
+								AND utilisateur_has_matiere.utilisateur_idUtilisateur = utilisateur.idUtilisateur 
+								AND utilisateur.idUtilisateur = devoir.utilisateur_idUtilisateur 
+								AND devoir.idDevoir = correctionDevoir.devoir_idDevoir 
+								AND utilisateur.idUtilisateur = ~
+								AND matiere.nomMatiere = ~
+								GROUP BY devoir.idDevoir');
+								
+define('GET_COMMENTAIRE_DEVOIR','SELECT commentaire 
+								FROM matiere, utilisateur_has_matiere, utilisateur, devoir, correctionDevoir 
+								WHERE matiere.idMatiere = utilisateur_has_matiere.matiere_idMatiere 
+								AND utilisateur_has_matiere.utilisateur_idUtilisateur = utilisateur.idUtilisateur 
+								AND utilisateur.idUtilisateur = devoir.utilisateur_idUtilisateur 
+								AND devoir.idDevoir = correctionDevoir.devoir_idDevoir 
+								AND utilisateur.idUtilisateur = ~
+								AND matiere.nomMatiere = ~
+								GROUP BY devoir.idDevoir');
+
+define('GET_LISTE_DEVOIR','SELECT devoir.idDevoir 
+								FROM matiere, utilisateur_has_matiere, utilisateur, devoir, correctionDevoir 
+								WHERE matiere.idMatiere = utilisateur_has_matiere.matiere_idMatiere 
+								AND utilisateur_has_matiere.utilisateur_idUtilisateur = utilisateur.idUtilisateur 
+								AND utilisateur.idUtilisateur = devoir.utilisateur_idUtilisateur 
+								AND devoir.idDevoir = correctionDevoir.devoir_idDevoir 
+								AND  utilisateur.idUtilisateur = ~
+								AND matiere.nomMatiere = ~');
+			
+define('GET_LISTE_MATIERE','SELECT matiere.idMatiere 
+								FROM utilisateur, correctionDevoir, devoir, devoir_has_matiere, matiere  
+								WHERE utilisateur.idUtilisateur = correctionDevoir.utilisateur_idUtilisateur 
+								AND correctionDevoir.devoir_idDevoir = devoir.idDevoir 
+								AND devoir.idDevoir = devoir_has_matiere.devoir_idDevoir 
+								AND devoir_has_matiere.matiere_idMatiere = matiere.idMatiere 
+								AND utilisateur.idUtilisateur = ~ GROUP BY matiere.idMatiere');
+
 /* SERIE DES INSERT */
 
 ?>
