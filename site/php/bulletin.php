b574c8bf33683945bcc0f351dc40636f66d45614
benjaminmille <benjamin.mille@hotmail.fr>
2014-02-19 16:44:32 +0100

    MAJ

--- site/php/bulletin.php
+++ site/php/bulletin.php
@@ -1,15 +1,9 @@
 <?php
 
-	include('fonctions.inc.php');
-	
-	$titrePage = "Bulletin";
-	$eleve = '1';
-	$classe = '1';
-	$trim = '1';
+
+
 	
 	include("../html/header.html");
-	include("fctRenvoieListeEleve.php");
-	include("fctRenvoieBulletin.php");
 	include("../html/stagiaire/bulletin.html");
 	include("../html/footer.html");
 
